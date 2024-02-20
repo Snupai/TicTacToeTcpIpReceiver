f800b86067fa6558d0b461a392aca4332d18b558
Yann <yannluca@naeher.eu>
2024-02-20 16:53:33 +0100

    Add project files.

---
+++ TicTacToeTcpIpReceiver/ShowOnDisplay.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+using System.Net.Sockets;
+using System.Net;
+using CommandLine;
+using System.Text;
+using System.ComponentModel.Design;
+
+namespace TicTacToeTcpIpReceiver
+{
+    internal class ShowOnDisplay
+    {
+
+    }
+}
